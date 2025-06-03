356d9c1685c5c8e851d928719ca44e80efe39b1d
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-06-03 15:21:37 -0300

    refactor: implement filter

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -36,12 +36,12 @@ export function Home() {
     };
 
     await itemsStorage.add(newItem);
-    await getItems();
+    await itemsByStatus();
   }
 
-  async function getItems() {
+  async function itemsByStatus() {
     try {
-      const response = await itemsStorage.get();
+      const response = await itemsStorage.getByStatus(filter);
       setItems(response);
     } catch (error) {
       console.log(error);
@@ -50,8 +50,8 @@ export function Home() {
   }
 
   useEffect(() => {
-    getItems();
-  }, []);
+    itemsByStatus();
+  }, [filter]);
 
   return (
     <View style={styles.container}>
